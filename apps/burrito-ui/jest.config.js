0588e9b78de81bcea6e4425fcb305bad5a015abf
Shaarang Tanpure <shaarang.1910@gmail.com>
2022-03-07 22:26:26 +1100

    initial commit

---
+++ apps/burrito-ui/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'burrito-ui',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/burrito-ui',
+};
